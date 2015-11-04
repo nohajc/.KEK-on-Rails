bac164702917f4a14761a02726c30370a5fbd8ac
Jan Noha <nohajc@gmail.com>
2015-11-04 19:25:08 +0100

    Updated tests/prime.kek, fixed another decl. error.

--- compiler/parser.cpp
+++ compiler/parser.cpp
@@ -204,6 +204,8 @@ StatmList * ZbDeklProm(Env env, bool isStatic) {
 		Var * var;
 		Expr * e;
 
+		deklProm(id, false, isStatic, env.clsEnv, env.mthEnv);
+
 		if (Symb.type == ASSIGN) {
 			Symb = readLexem();
 			var = new Var(adrProm(id, env.clsEnv, env.mthEnv), NULL, false);
